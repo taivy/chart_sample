d4e6339b18f1ad8217d62c435b69e018dfc31163
Lavka <lavka@MacBook-Pro-Lavka.local>
2023-04-18 18:27:56 +0300

    src

---
+++ src/script.js
@@ -0,0 +1,54 @@
+import Chart from 'chart.js/auto'
+
+(async function() {
+    const data = {
+        datasets: [{
+          label: 'Scatter Dataset',
+          data: [{
+            x: 333.74,
+            y: 60
+          }, {
+            x: 588.38,
+            y: 96
+          }, {
+            x: 985.65,
+            y: 120
+          }, {
+            x: 1732.2,
+            y: 138
+          }, {
+            x: 2887.7,
+            y: 156
+          }],
+          backgroundColor: 'rgb(255, 99, 132)',
+          showLine: true,
+        }],
+      };
+
+
+  new Chart(
+    document.getElementById('chart'),
+    {
+      type: 'scatter',
+      data: data,
+      options: {
+        scales: {
+          x: {
+            type: 'linear',
+            position: 'bottom',
+            beginAtZero: true,
+            ticks: {
+                stepSize: 200
+            },
+          },
+          y: {
+            beginAtZero: true,
+            ticks: {
+                stepSize: 20
+            },
+          }
+        }
+      }
+    }
+  );
+})();
