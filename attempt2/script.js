074bf41dc03e917943b90e6e185da4385fb5477c
Lavka <lavka@MacBook-Pro-Lavka.local>
2023-04-18 20:17:28 +0300

    remove legend

--- attempt2/script.js
+++ attempt2/script.js
@@ -1,49 +1,54 @@
 const ctx = document.getElementById('chart');
 
 const data = {
-    datasets: [{
-        label: 'Scatter Dataset',
-        data: [{
-            x: 333.74,
-            y: 60
-        }, {
-            x: 588.38,
-            y: 96
-        }, {
-            x: 985.65,
-            y: 120
-        }, {
-            x: 1732.2,
-            y: 138
-        }, {
-            x: 2887.7,
-            y: 156
-        }],
-        backgroundColor: 'rgb(255, 99, 132)',
-        showLine: true,
-    }],
+  datasets: [{
+      data: [{
+          x: 333.74,
+          y: 60
+      }, {
+          x: 588.38,
+          y: 96
+      }, {
+          x: 985.65,
+          y: 120
+      }, {
+          x: 1732.2,
+          y: 138
+      }, {
+          x: 2887.7,
+          y: 156
+      }],
+      backgroundColor: 'rgb(255, 99, 132)',
+      showLine: true,
+  }],
 };
 
 var myChart = new Chart(ctx, { 
     type: 'scatter',
     data: data,
     options: {
-        scales: {
+      scales: {
         x: {
-            type: 'linear',
-            position: 'bottom',
-            beginAtZero: true,
-            max: 3800,
-            ticks: {
-                stepSize: 200
-            },
+          type: 'linear',
+          position: 'bottom',
+          beginAtZero: true,
+          max: 3800,
+          ticks: {
+              stepSize: 200
+          },
         },
         y: {
-            beginAtZero: true,
-            max: 240,
-            ticks: {
-                stepSize: 20
-            },
-        }}
+          beginAtZero: true,
+          max: 240,
+          ticks: {
+              stepSize: 20
+          },
+        }
+      },
+      plugins: {
+        legend: {
+          display: false,
+        }
+      }
     }
 });
